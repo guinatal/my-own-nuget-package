888ca2e3c1b3b8bbf61d6a64a8a652eaf6173958
Guilherme Natal de mello <Gui_Natal@hotmail.com>
2023-09-04 08:27:04 +0100

    frist commit

---
+++ My.First.Project/Exceptions/InvalidUserNameException.cs
@@ -0,0 +1,7 @@
+﻿namespace My.First.Project.Exceptions;
+
+public class InvalidUserNameException : Exception
+{
+    public InvalidUserNameException(string message) : base(message) { }
+    public InvalidUserNameException(string message, Exception innerException) : base(message, innerException) { }
+}
